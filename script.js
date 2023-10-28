5a095120b4c2654ce60034e06581ceac1e010042
GuilhermeDeSanti <guilhermehdesanti@gmail.com>
2023-10-28 11:58:44 -0300

    Completo

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 let person = "Guilherme";
+var exp = 6030;
 
-console.log("Bem vindo" + person)
+  if (exp <= 1000) {
+    var exp = "Ferro";
+  }
+  if (exp > 1001 && exp <= 2000) {
+    var exp = "Bronze";
+  }
+  if (exp <= 2001 && exp <= 5000) {
+    var exp = "Prata";
+  }
+  if (exp = 6001 <= 7000) {
+    var exp = "Ouro";
+  }
+  if (exp >= 7001 && 8000) {
+    var exp = "Platina";
+  }
+  if (exp <= 8001 && 9000) {
+    var exp = "Ascendente";
+  }
+  if (exp <= 9001 && 10000) {
+    var exp = "Imortal";
+  }
+  if (exp > 10001) {
+    var exp = "Radiante";
+  }
+  
+console.log("O herói de nome " + person + " Está no nível " + exp)
