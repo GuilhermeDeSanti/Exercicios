b4dd1b13ccddabc747cee3d94fa34f3e2664f3a1
GuilhermeDeSanti <guilhermehdesanti@gmail.com>
2023-10-28 13:14:39 -0300

    Completo

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 let person = "Guilherme";
-var exp = 6030;
+var exp = 30001;
 
-  if (exp <= 1000) {
+  while(exp <= 1000) {
     var exp = "Ferro";
   }
-  if (exp > 1001 && exp <= 2000) {
+  while(exp >= 1001 && exp <= 2000) {
     var exp = "Bronze";
   }
-  if (exp <= 2001 && exp <= 5000) {
+  while(exp >= 2001 && exp <= 5000) {
     var exp = "Prata";
   }
-  if (exp = 6001 <= 7000) {
+  while(exp >= 6001 && exp <= 7000) {
     var exp = "Ouro";
   }
-  if (exp >= 7001 && 8000) {
+  while(exp >= 7001 && exp <= 8000) {
     var exp = "Platina";
   }
-  if (exp <= 8001 && 9000) {
+  while(exp >= 8001 && exp <= 9000) {
     var exp = "Ascendente";
   }
-  if (exp <= 9001 && 10000) {
+  while(exp >= 9001 && exp <= 10000) {
     var exp = "Imortal";
   }
-  if (exp > 10001) {
-    var exp = "Radiante";
+  while(exp >= 10001) {
+      var exp = "Radiante";
   }
   
 console.log("O herói de nome " + person + " Está no nível " + exp)
